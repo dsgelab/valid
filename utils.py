226460613d83b72ba9f87da2ee02227e945df862
detrokir <kira.detrois@helsinki.fi>
2024-10-31 12:10:37 +0100

    updated utils

--- utils.py
+++ utils.py
@@ -1,20 +1,15 @@
-try:
-    from google.cloud import bigquery
-    client = bigquery.Client()
-except ImportError:
-    pass
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import os
 
+import os
 def make_dir(dir_path):
     if not os.path.exists(dir_path): os.makedirs(dir_path)
 
 def print_count(data):
     print("{:,} individuals with {:,} rows".format(data.FINNGENID.nunique(), data.shape[0]))
 
+import matplotlib.pyplot as plt
+import seaborn as sns
 def plot_data(data, fg_id):
     crnt_data = data.loc[data.FINNGENID == fg_id]
     fig, ax = plt.subplots()
@@ -30,7 +25,25 @@ def plot_data(data, fg_id):
                   color="black")
     display(crnt_data)
 
+try:
+    from google.cloud import bigquery
+except ImportError:
+    pass
+# From Mathias for FinnGen BigQuery
+def query_to_df(query, **job_config_kwargs):
+    client = bigquery.Client()
+    job_config = bigquery.QueryJobConfig(**job_config_kwargs)
+    query_result = client.query(query, job_config=job_config)
+    df = pd.DataFrame([dict(row) for row in query_result])
+    df = parse_dates(df)
+    return(df)
+def parse_dates(df):
+    for col in df.columns:
+        if cols.endswith("_DATETIME"): df[col] = pd.to_datetime(df[col])
+    return df
+
 # https://stackoverflow.com/questions/7370801/how-do-i-measure-elapsed-time-in-python
+import time
 class Timer:
     def __init__(self):
         self.start_time = time.time()
