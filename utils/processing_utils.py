6339fa3ae1bcc00a163afa6163f21070eeb0ccf8
detrokir <kira.detrois@helsinki.fi>
2025-03-14 12:07:40 +0200

    moved some functions

--- utils/processing_utils.py
+++ utils/processing_utils.py
@@ -7,8 +7,11 @@ def get_abnorm_func_based_on_name(lab_name):
     if lab_name == "hba1c": return(hba1c_abnorm)
     if lab_name == "ldl": return(ldl_abnorm)
     if lab_name == "egfr" or lab_name == "krea": return(egfr_abnorm)
-    if lab_name == "cyst": return(cystc_abnorm)
+    if lab_name == "cyst" or lab_name =="cystc": return(cystc_abnorm)
     if lab_name == "gluc" or lab_name=="fgluc": return(gluc_abnorm)
+    if lab_name == "alat": return(alat_abnorm)
+    if lab_name == "asat": return(asat_abnorm)
+
     else: raise("Sorry, no function for this lab name.")
 
 """Individual ABNORMity with grey area 2.5-4"""
@@ -21,8 +24,19 @@ def tsh_abnorm(data, value_col_name="VALUE"):
 
 def gluc_abnorm(data, value_col_name="VALUE"):
     data.loc[:,"ABNORM_CUSTOM"] = 0
-    data.loc[data[value_col_name] > 6, "ABNORM_CUSTOM"] = 1
+    data.loc[data[value_col_name] >= 7, "ABNORM_CUSTOM"] = 1
     data.loc[data[value_col_name] < 4, "ABNORM_CUSTOM"] = -1
+    return(data)
+
+"""Based on what andrea sent."""
+def alat_abnorm(data, value_col_name="VALUE"):
+    data.loc[:,"ABNORM_CUSTOM"] = 0
+    data.loc[data[value_col_name] > 45, "ABNORM_CUSTOM"] = 1
+    return(data)
+"""Based on what andrea sent."""
+def asat_abnorm(data, value_col_name="VALUE"):
+    data.loc[:,"ABNORM_CUSTOM"] = 0
+    data.loc[data[value_col_name] > 35, "ABNORM_CUSTOM"] = 1
     return(data)
 
 """Abnormality for cystatin c"""
@@ -58,9 +72,6 @@ def ldl_abnorm(data):
     data.loc[data.VALUE > 5.3,"ABNORM_CUSTOM"] = 1
     data.loc[np.logical_and(data.EVENT_AGE <= 49, data.VALUE > 4.7),"ABNORM_CUSTOM"] = 1
     data.loc[np.logical_and(data.EVENT_AGE <= 29, data.VALUE > 4.3),"ABNORM_CUSTOM"] = 1
-
-    return(data)   
-
     return(data)
 
 """Based on transformed kreatinine to eGFR."""
@@ -89,16 +100,17 @@ def add_measure_counts(data):
     data = pd.merge(data, n_measures, on="FINNGENID", how="left")
     return(data)
 
+
 def add_set(unique_data, test_pct=0.1, valid_pct=0.1):
     """Adds SET column to data based on random split of individuals.
        Data passed must be unique data with only one row per individual."""
     data_train, data_rest = train_test_split(unique_data, shuffle=True, random_state=3291, test_size=(valid_pct+test_pct), train_size=1-(valid_pct+test_pct), stratify=unique_data.y_DIAG)
-    print(f"N rows {len(data_train)}   N indvs {len(set(data_train.FINNGENID))}  N cases {sum(data_train.y_DIAG)} pct cases {round(sum(data_train.y_DIAG)/len(data_train), 2)}")
-    print(f"N rows {len(data_rest)}   N indvs {len(set(data_rest.FINNGENID))}  N cases {sum(data_rest.y_DIAG)} pct cases {round(sum(data_rest.y_DIAG)/len(data_rest), 2)}")
+    print(f"N rows train {len(data_train)}   N indvs train {len(set(data_train.FINNGENID))}  N cases train {sum(data_train.y_DIAG)} pct cases {round(sum(data_train.y_DIAG)/len(data_train), 2)}")
+    print(f"N rows rest {len(data_rest)}   N indvs rest{len(set(data_rest.FINNGENID))}  N cases {sum(data_rest.y_DIAG)} pct cases {round(sum(data_rest.y_DIAG)/len(data_rest), 2)}")
 
     data_valid, data_test = train_test_split(data_rest, shuffle=True, random_state=391, test_size=test_pct/(test_pct+valid_pct), train_size=valid_pct/(test_pct+valid_pct), stratify=data_rest.y_DIAG)
-    print(f"N rows {len(data_valid)}   N indvs {len(set(data_valid.FINNGENID))}  N cases {sum(data_valid.y_DIAG)} pct cases {round(sum(data_valid.y_DIAG)/len(data_valid), 2)}")
-    print(f"N rows {len(data_test)}   N indvs {len(set(data_test.FINNGENID))}  N cases {sum(data_test.y_DIAG)} pct cases {round(sum(data_test.y_DIAG)/len(data_test), 2)}")
+    print(f"N rows valid {len(data_valid)}   N indvs valid {len(set(data_valid.FINNGENID))}  N cases {sum(data_valid.y_DIAG)} pct cases {round(sum(data_valid.y_DIAG)/len(data_valid), 2)}")
+    print(f"N rows test {len(data_test)}   N indvs test {len(set(data_test.FINNGENID))}  N cases {sum(data_test.y_DIAG)} pct cases {round(sum(data_test.y_DIAG)/len(data_test), 2)}")
 
     unique_data.loc[unique_data.FINNGENID.isin(data_train.FINNGENID),"SET"] = 0
     unique_data.loc[unique_data.FINNGENID.isin(data_valid.FINNGENID),"SET"] = 1
