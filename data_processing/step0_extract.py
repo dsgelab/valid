75023cec61e01827c81bad0daeeb277763fc36d2
detrokir <kira.detrois@helsinki.fi>
2025-03-26 11:06:08 +0200

    switch to parquet and ploars + new extract column data R13 kanta lab v2

--- data_processing/step0_extract.py
+++ data_processing/step0_extract.py
@@ -1,16 +1,16 @@
-# Utils
 import sys
 sys.path.append(("/home/ivm/valid/scripts/utils/"))
-from utils import *
+from general_utils import Timer, get_date, get_datetime, make_dir, init_logging, query_to_df
 # Standard stuff
 import pandas as pd
+import polars as pl
 # Logging and input
 import logging
 logger = logging.getLogger(__name__)
 import argparse
 
 """Get data for a specific OMOP concept ID from the kanta lab data. Returns a panda with data."""
-def get_omop_id_data(omop_concept_ids = ["3020564"],
+def get_omop_id_data_bq(omop_concept_ids = ["3020564"],
                      table = f"`finngen-production-library.sandbox_tools_r12.kanta_r12_v1`",
                      columns = ["FINNGENID", "SEX", "EVENT_AGE", "TEST_OUTCOME", "TEST_OUTCOME_IMPUTED", "MEASUREMENT_VALUE_HARMONIZED",  "MEASUREMENT_UNIT_HARMONIZED", "MEASUREMENT_VALUE", "MEASUREMENT_UNIT", "APPROX_EVENT_DATETIME"]):
     omop_concept_str = ",".join(f"'{id}'" for id in omop_concept_ids)
@@ -23,6 +23,31 @@ def get_omop_id_data(omop_concept_ids = ["3020564"],
     timer = Timer()
     data = query_to_df(query)
     logger.info("Time import: "+timer.get_elapsed())
+    return(data)
+
+"""Get data for a specific OMOP concept ID from the kanta lab data. Returns a panda with data."""
+def get_orig_omop_id_data_parquet(omop_concept_id = "3020564",
+                                  table = "/finngen/library-red/finngen_R13/kanta_lab_1.0/finngen_R12_kanta_lab_2.0-rc.1.parquet",
+                                  columns = ["FINNGENID", "SEX", "EVENT_AGE", "APPROX_EVENT_DATETIME", "MEASUREMENT_VALUE", "MEASUREMENT_UNIT",  "TEST_OUTCOME"]):
+    data = pl.DataFrame(pd.read_parquet(table, 
+                                        filers=[("OMOP_CONCEPT_ID", "==", omop_concept_id)],
+                                        columns=columns))
+    return(data)
+
+"""Get data for a specific OMOP concept ID from the kanta lab data. Returns a panda with data."""
+def get_extract_omop_id_data_parquet(omop_concept_id = "3020564",
+                                     table = "/finngen/pipeline/DATA_IMPORT/fg-3/kanta/finngen_R12_kanta_lab_2.0-rc.1.parquet",
+                                     columns = ["FINNGENID", "SEX", "EVENT_AGE", "APPROX_EVENT_DATETIME", "TEST_OUTCOME", "MEASUREMENT_VALUE_EXTRACTED", "IS_VALUE_EXTRACTED"]):
+    data = pl.DataFrame(pd.read_parquet(table, 
+                                        filers=[("OMOP_CONCEPT_ID", "==", omop_concept_id)],
+                                        columns=columns))
+    data = (data.filter(pl.col("IS_VALUE_EXTRACTED") == "1")
+                .with_columns(MEASUREMENT_VALUE=pl.col("MEASUREMENT_VALUE_EXTRACTED"),
+                              MEASUREMENT_UNIT=None,
+                              MEASUREMENT_VALUE_HARMONIZED=None,
+                              MEASUREMENT_UNIT_HARMONIZED=None,
+                              TEST_OUTCOME_IMPUTED=None)
+                .drop(["MEASUREMENT_VALUE_EXTRACTED", "IS_VALUE_EXTRACTED"]))
     return(data)
 
 """Setting up the parser arguments."""
@@ -48,14 +73,16 @@ if __name__ == "__main__":
     
     #### Data processing
     ## Raw data
-    data = get_omop_id_data(omop_concept_ids=[args.omop], columns=["FINNGENID", "SEX", "EVENT_AGE", "TEST_OUTCOME", "TEST_OUTCOME_IMPUTED", "MEASUREMENT_VALUE_HARMONIZED", "MEASUREMENT_UNIT_HARMONIZED", "MEASUREMENT_VALUE", "MEASUREMENT_UNIT", "APPROX_EVENT_DATETIME"])
-    print(data)
-    print(data.loc[data.MEASUREMENT_VALUE.isnull()])
+    data = get_orig_omop_id_data_parquet(omop_concept_ids=args.omop, 
+                                         columns=["FINNGENID", "SEX", "EVENT_AGE", "TEST_OUTCOME", "TEST_OUTCOME_IMPUTED", "MEASUREMENT_VALUE_HARMONIZED", "MEASUREMENT_UNIT_HARMONIZED", "MEASUREMENT_VALUE", "MEASUREMENT_UNIT", "APPROX_EVENT_DATETIME"])
+    extract_data = get_extract_omop_id_data_parquet(omop_concept_ids=args.omop)
+    extract_data = extract_data.select(data.columns)
+    all_data = pl.concat([data, extract_data])
     ## Sorting
-    data = data.sort_values(["FINNGENID", "APPROX_EVENT_DATETIME"], ascending=False)
-    data = data.rename(columns={"APPROX_EVENT_DATETIME": "DATE", "MEASUREMENT_VALUE": "VALUE", "TEST_OUTCOME": "ABNORM", "MEASUREMENT_UNIT": "UNIT", "MEASUREMENT_VALUE_HARMONIZED": "VALUE_FG", "MEASUREMENT_UNIT_HARMONIZED": "UNIT_FG", "TEST_OUTCOME_IMPUTED":"ABNORM_FG"})
-    data = data[["FINNGENID", "SEX", "EVENT_AGE", "DATE", "VALUE", "UNIT", "ABNORM", "VALUE_FG", "UNIT_FG", "ABNORM_FG"]]
+    data = data.sort(["FINNGENID", "APPROX_EVENT_DATETIME"], descending=True)
+    data = data.rename({"APPROX_EVENT_DATETIME": "DATE", "MEASUREMENT_VALUE": "VALUE", "TEST_OUTCOME": "ABNORM", "MEASUREMENT_UNIT": "UNIT", "MEASUREMENT_VALUE_HARMONIZED": "VALUE_FG", "MEASUREMENT_UNIT_HARMONIZED": "UNIT_FG", "TEST_OUTCOME_IMPUTED":"ABNORM_FG"})
+    data = data.select(["FINNGENID", "SEX", "EVENT_AGE", "DATE", "VALUE", "UNIT", "ABNORM", "VALUE_FG", "UNIT_FG", "ABNORM_FG"])
 
     ## Saving
-    data.to_csv(args.res_dir + file_name + ".csv", sep=",", index=False)
+    data.write_parquet(args.res_dir + file_name + ".parquet")
     logger.info("Time total: "+timer.get_elapsed())
