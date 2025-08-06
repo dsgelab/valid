6657ba670f9c7bc78afeb2bcf8cdba2039fdca7a
detrokir <kira.detrois@helsinki.fi>
2025-08-06 12:01:57 +0300

    added abnormality functions

--- utils/processing_utils.py
+++ utils/processing_utils.py
@@ -19,29 +19,51 @@ def generate_random_date(year):
         
     # Return the sampled random date
     return datetime(year, month, day).date()
+
+def egfr_ckdepi2021_transform(data,
+                              value_col_name="VALUE"):
+    fem_k = 0.7
+    male_k = 0.9
+    fem_alpha = -0.241
+    male_alpha = -0.302
+    data = data.with_columns((pl.col(value_col_name)/88.4).alias(value_col_name))
+    data=data.with_columns(
+        pl.when(pl.col.SEX=="male")
+        .then(142*(pl.min_horizontal(pl.col(value_col_name)/male_k, 1)**male_alpha)*(pl.max_horizontal(pl.col(value_col_name)/male_k, 1)**(-1.2))*(0.9938**pl.col.EVENT_AGE))
+        .otherwise(142*(pl.min_horizontal(pl.col(value_col_name)/fem_k, 1)**fem_alpha)*(pl.max_horizontal(pl.col(value_col_name)/male_k, 1)**(-1.2))*(0.9938**pl.col.EVENT_AGE)*1.012)
+        .alias(value_col_name)
+    )
     
-def egfr_transform(data):
-    data = data.to_pandas()
-    data.loc[np.logical_and(data["VALUE"] <= 62, data["SEX"] == "female"), "VALUE_TRANSFORM"]=((data.loc[np.logical_and(data["VALUE"] <= 62, data["SEX"] == "female"), "VALUE"]/61.9 )**(-0.329))*(0.993**data.loc[np.logical_and(data["VALUE"] <= 62, data["SEX"] == "female"), "EVENT_AGE"])*144
-    data.loc[np.logical_and(data["VALUE"] > 62, data["SEX"] == "female"), "VALUE_TRANSFORM"]=((data.loc[np.logical_and(data["VALUE"] > 62, data["SEX"] == "female"), "VALUE"]/61.9 )**(-1.209))*(0.993**data.loc[np.logical_and(data["VALUE"] > 62, data["SEX"] == "female"), "EVENT_AGE"])*144
-    data.loc[np.logical_and(data["VALUE"] <= 80, data["SEX"] == "male"), "VALUE_TRANSFORM"]=((data.loc[np.logical_and(data["VALUE"] <= 80, data["SEX"] == "male"), "VALUE"]/79.6 )**(-0.411))*(0.993**data.loc[np.logical_and(data["VALUE"] <= 80, data["SEX"] == "male"), "EVENT_AGE"])*141
-    data.loc[np.logical_and(data["VALUE"] > 80, data["SEX"] == "male"), "VALUE_TRANSFORM"]=((data.loc[np.logical_and(data["VALUE"] > 80, data["SEX"] == "male"), "VALUE"]/79.6 )**(-1.209))*(0.993**data.loc[np.logical_and(data["VALUE"] > 80, data["SEX"] == "male"), "EVENT_AGE"])*141
-    data = data.assign(VALUE = data["VALUE_TRANSFORM"])
-    data = data.drop("VALUE_TRANSFORM", axis=1)
+    return(data)
+
+def cystc_ckdepi2012_transform(data,
+                              value_col_name="VALUE"):
+    data=data.with_columns(
+        pl.when(pl.col.SEX=="male")
+        .then(133*(pl.min_horizontal(pl.col(value_col_name)/0.8, 1)**(-0.499))*(pl.max_horizontal(pl.col(value_col_name)/0.8, 1)**(-1.328))*(0.996**pl.col.EVENT_AGE))
+        .otherwise(133*(pl.min_horizontal(pl.col(value_col_name)/0.8, 1)**(-0.499))*(pl.max_horizontal(pl.col(value_col_name)/0.8, 1)**(-1.328))*(0.996**pl.col.EVENT_AGE)*0.932)
+        .alias(value_col_name)
+    )
     
-    return(pl.DataFrame(data))
+    return(data)
     
     
 def get_abnorm_func_based_on_name(lab_name,
                                   extra_choice=""):
-    if lab_name == "tsh": return(tsh_abnorm)
+    if lab_name == "tsh": 
+        if extra_choice == "simple": return(tsh_abnorm_simple)
+        if extra_choice == "multi": return(tsh_abnorm_complex)
     if lab_name == "hba1c": 
-        if extra_choice == "mild": return(hba1c_mild_abnorm)
+        if extra_choice == "soft": return(hba1c_mild_abnorm)
         if extra_choice == "strong": return(hba1c_strong_abnorm)
     if lab_name == "ldl": return(ldl_abnorm)
+    if lab_name == "ana": return(ana_abnorm)
+    if lab_name == "uacr": return(uacr_abnorm)
     if lab_name == "egfr" or lab_name == "krea": 
-        if extra_choice == "age":
-            return(egfr_transform)
+        if extra_choice == "herold-full":
+            return(lambda x, y: egfr_herold(x,y, strict=True))
+        if extra_choice == "herold-part":
+            return(lambda x, y: egfr_herold(x,y, strict=False))
         if extra_choice == "KDIGO-strict":
             return(lambda x, y: egfr_kdigo_abnorm(x, y, strict=True))
         if extra_choice == "KDIGO-soft":
@@ -52,6 +74,8 @@ def get_abnorm_func_based_on_name(lab_name,
     if lab_name == "gluc" or lab_name=="fgluc": return(gluc_abnorm)
     if lab_name == "alat": return(alat_abnorm)
     if lab_name == "asat": return(asat_abnorm)
+    if lab_name == "t4": return(t4_abnorm)
+    if lab_name == "t3": return(t3_abnorm)
 
     else: raise ValueError("Sorry, no function for this lab name.")
         
@@ -73,15 +97,63 @@ def egfr_kdigo_abnorm(data,
         print(data["ABNORM_CUSTOM"].value_counts())
     return(data)
 
-"""Individual ABNORMity with grey area 2.5-4"""
-def tsh_abnorm(data, value_col_name="VALUE"):
-    data = data.to_pandas()
+def tsh_abnorm_complex(data, value_col_name="VALUE"):
+    data = data.with_columns(
+                pl.when(pl.col(value_col_name)>=4)
+                .then(1)
+                .when(pl.col(value_col_name)<0.4)
+                .then(-1)
+                .otherwise(0)
+        .alias("ABNORM_CUSTOM")
+    )
+    print(data["ABNORM_CUSTOM"].value_counts())
+    return(data)
 
-    data.loc[data[value_col_name] < 4,"ABNORM_CUSTOM"] = 0
-    data.loc[data[value_col_name] > 2.5,"ABNORM_CUSTOM"] = 0.5
-    data.loc[data[value_col_name] >= 4,"ABNORM_CUSTOM"] = 1
-    data.loc[data[value_col_name] < 0.4,"ABNORM_CUSTOM"] = -1
-    return(pl.DataFrame(data))
+def tsh_abnorm_simple(data, value_col_name="VALUE"):
+    data = data.with_columns(
+                pl.when(pl.col(value_col_name)>4)
+                .then(1)
+                .otherwise(0)
+        .alias("ABNORM_CUSTOM")
+    )
+    return(data)
+
+def uacr_abnorm(data, value_col_name="VALUE"):
+    data = data.with_columns(
+                pl.when(pl.col(value_col_name)<3)
+                .then(pl.lit(0))
+                .when((pl.col(value_col_name)>3)&(pl.col(value_col_name)<=30))
+                .then(pl.lit(1))
+                .when((pl.col(value_col_name)>30)&(pl.col(value_col_name)<=220))
+                .then(pl.lit(2))
+                .otherwise(pl.lit(3))
+        .alias("ABNORM_CUSTOM")
+    )
+    print(data["ABNORM_CUSTOM"].value_counts())
+    return(data)
+def t4_abnorm(data, value_col_name="VALUE"):
+    data = data.with_columns(
+                pl.when(pl.col(value_col_name)>23)
+                .then(1)
+                .when(pl.col(value_col_name)<11)
+                .then(-1)
+                .otherwise(0)
+        .alias("ABNORM_CUSTOM")
+    )
+    print(data["ABNORM_CUSTOM"].value_counts())
+    return(data)
+    
+def t3_abnorm(data, value_col_name="VALUE"):
+    data = data.with_columns(
+                pl.when(pl.col(value_col_name)>6.3)
+                .then(1)
+                .when(pl.col(value_col_name)<2.6)
+                .then(-1)
+                .otherwise(0)
+        .alias("ABNORM_CUSTOM")
+    )
+    print(data["ABNORM_CUSTOM"].value_counts())
+    return(data)
 
 def gluc_abnorm(data, value_col_name="VALUE"):
     data = data.to_pandas()
@@ -160,6 +232,35 @@ def ldl_abnorm(data):
     data.loc[np.logical_and(data.EVENT_AGE <= 29, data.VALUE > 4.3),"ABNORM_CUSTOM"] = 1
     return(pl.DataFrame(data))
 
+
+def ana_abnorm(data, value_col_name="VALUE"):
+    data = data.with_columns(pl.when(pl.col(value_col_name)>400).then(1).otherwise(0).alias("ABNORM_CUSTOM"))
+    return(pl.DataFrame(data))
+
+"""Based on transformed kreatinine to eGFR. Herold et al. 2024 age-specific cut-offs"""
+def egfr_herold(data, value_col_name="VALUE_TRANSFORM", strict=False):
+    if strict:
+        return(data.with_columns(
+            pl.when((pl.col.EVENT_AGE<40)&(pl.col(value_col_name)<60)).then(1)
+            .when((pl.col.EVENT_AGE>=30)&(pl.col.EVENT_AGE<40)&(pl.col(value_col_name)<75)).then(1)
+            .when((pl.col.EVENT_AGE>=40)&(pl.col.EVENT_AGE<50)&(pl.col(value_col_name)<70)).then(1)
+            .when((pl.col.EVENT_AGE>=50)&(pl.col.EVENT_AGE<60)&(pl.col(value_col_name)<60)).then(1)
+            .when((pl.col.EVENT_AGE>=60)&(pl.col.EVENT_AGE<70)&(pl.col(value_col_name)<50)).then(1)
+            .when((pl.col.EVENT_AGE>=70)&(pl.col.EVENT_AGE<80)&(pl.col(value_col_name)<40)).then(1)
+            .when((pl.col.EVENT_AGE>=80)&(pl.col(value_col_name)<35)).then(1)
+            .otherwise(0)
+            .alias("ABNORM_CUSTOM")
+        ))
+    else:
+        return(data.with_columns(
+            # pl.when((pl.col.EVENT_AGE<60)&(pl.col(value_col_name)<60)).then(1)
+            # .when((pl.col.EVENT_AGE>=40)&(pl.col.EVENT_AGE<50)&(pl.col(value_col_name)<70)).then(1)
+            # .when((pl.col.EVENT_AGE>=50)&(pl.col.EVENT_AGE<60)&(pl.col(value_col_name)<60)).then(1)
+            pl.when((pl.col.EVENT_AGE.floor()<60)&(pl.col(value_col_name)<60)).then(1)
+            .when((pl.col.EVENT_AGE.floor()>=60)&(pl.col(value_col_name)<50)).then(1)
+            .otherwise(0)
+            .alias("ABNORM_CUSTOM")
+        ))
 """Based on transformed kreatinine to eGFR."""
 def egfr_abnorm(data, value_col_name="VALUE_TRANSFORM"):
     data = data.to_pandas()
@@ -201,7 +302,7 @@ def add_set(unique_data,
                                                  random_state=3291, 
                                                  test_size=round((valid_pct+test_pct),2), 
                                                  train_size=round(1-(valid_pct+test_pct),2), 
-                                                 stratify=unique_data["y_DIAG"])
+                                                 stratify=unique_data["y_MEAN_ABNORM"])
         new_test_size = round(test_pct/(test_pct+valid_pct),2)
         new_train_size = round(valid_pct/(test_pct+valid_pct),2)
     else:
@@ -213,14 +314,14 @@ def add_set(unique_data,
                                              random_state=391, 
                                              test_size=new_test_size, 
                                              train_size=new_train_size, 
-                                             stratify=data_rest["y_DIAG"])
+                                             stratify=data_rest["y_MEAN_ABNORM"])
     if test_pct == 0:
         unique_data = unique_data.with_columns(
                             SET=pl.when(pl.col("FINNGENID").is_in(data_valid["FINNGENID"])).then(0)
                                    .when(pl.col("FINNGENID").is_in(data_test["FINNGENID"])).then(1)
                                    .otherwise(None)
     )
-        print(f"N rows train {len(data_valid)}   N indvs train {len(set(data_valid["FINNGENID"]))}  N cases train {sum(data_valid["y_DIAG"])} pct cases {round(sum(data_valid["y_DIAG"])/len(data_valid), 2)}")
+        print(f"N rows train {len(data_valid)}   N indvs train {len(set(data_valid["FINNGENID"]))}  N mean abnorm train {sum(data_valid["y_MEAN_ABNORM"])} pct mean abnorm {round(sum(data_valid["y_MEAN_ABNORM"])/len(data_valid), 2)}")
 
     else:
         unique_data = unique_data.with_columns(
@@ -229,7 +330,7 @@ def add_set(unique_data,
                .when(pl.col("FINNGENID").is_in(data_test["FINNGENID"])).then(2)
                .otherwise(None)
         )
-        print(f"N rows train {len(data_train)}   N indvs train {len(set(data_train["FINNGENID"]))}  N cases train {sum(data_train["y_DIAG"])} pct cases {round(sum(data_train["y_DIAG"])/len(data_train), 2)}")
+        print(f"N rows train {len(data_train)}   N indvs train {len(set(data_train["FINNGENID"]))}  N mean abnorm train {sum(data_train["y_MEAN_ABNORM"])} pct mean abnorm {round(sum(data_train["y_MEAN_ABNORM"])/len(data_train), 2)}")
 
     print(unique_data.select(pl.col("SET")).to_series().value_counts())
     return(pl.DataFrame(unique_data))
