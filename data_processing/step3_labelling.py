dfcf7076b76e27573740e9d8d3fec09a9b86159b
detrokir <kira.detrois@helsinki.fi>
2025-08-06 11:59:55 +0300

    updated labelling with exclusions before and test and training in one

--- data_processing/step3_labelling.py
+++ data_processing/step3_labelling.py
@@ -1,9 +1,10 @@
 # Utils
 import sys
 sys.path.append(("/home/ivm/valid/scripts/utils/"))
-from general_utils import get_date, get_datetime, make_dir, init_logging, Timer, logging_print
-from labeling_utils import get_lab_data, log_print_n, label_cases_and_controls, remove_prior_diags, remove_only_icd_or_atc_diags, remove_buffer_abnorm, remove_last_value_abnormal, remove_age_outliers, remove_other_exclusion, get_low_bmi_indvs
+from general_utils import get_date, get_datetime, make_dir, init_logging, Timer, logging_print, read_file
+from labeling_utils_new import get_lab_data, log_print_n, label_cases_and_controls,  remove_age_outliers
 from diag_utils import relabel_data_diag
+from processing_utils import add_set
 
 # Standard stuff
 import numpy as np
@@ -23,10 +24,6 @@ def get_parser_arguments():
     parser = argparse.ArgumentParser()
     # Data paths
     parser.add_argument("--data_path_full", type=str, help="Path to data.", default="/home/ivm/valid/data/processed_data/step1_clean/")
-    parser.add_argument("--diags_path", type=str, help="Path to data.", default="/home/ivm/valid/data/processed_data/step2_diags/")
-    parser.add_argument("--diags_file_name", type=str, help="File name of diagnosis file.")
-    parser.add_argument("--exclude_file_name", type=str, help="File name of excluded individuals.")
-
     parser.add_argument("--fg_ver", type=str, default="R12", help="Which FinnGen release [options: R12 and R13]")
 
     # Saving info
@@ -35,14 +32,7 @@ def get_parser_arguments():
     parser.add_argument("--version", type=str, help="Versioning of test set for shorter file names.", required=True)
 
     # Settings for data-processing
-    parser.add_argument("--exclude_diags", type=int, default=0, help="Whether to exclude individuals with prior diags or not. 0 = no, 1 = yes")
-    parser.add_argument("--data_diag_excl", type=int, default=0, help="Whether to remove individuals with only a data-based diagnosis prior to the start.")
-    parser.add_argument("--shorten_diag_pred", type=int, default=0, help="Whether to relabel all diags based on a different window than the exclusion one.")
-    parser.add_argument("--diff_days", type=int, default=None, help="Minimum number of days between measurements needed for diagnosis. Needed when relabeling data-based diagnoses to after start-date.")
-
-    parser.add_argument("--remove_low_bmi", type=int, help="Whether to remove individuals with a BMI<18.5", default=0)
-    parser.add_argument("--no_prior_diag_start", type=int, help="Whether to only consider cases who has first abnormal after the baseline. Removing those with first data-base abnormal before.", default=0)
-    parser.add_argument("--abnorm_type", type=str, required=True, help="[Options for eGFR: age, KDIGO-strict, KDIGO-soft for HbA1c: strong and soft.]. age: \
+    parser.add_argument("--abnorm_type", type=str, default="", help="[Options for eGFR: age, KDIGO-strict, KDIGO-soft for HbA1c: strong and soft.]. age: \
                         egfr abnormality based on age. KDIGO-stric: <60 for all. KDIGO-soft: <60 but with 60-65 allowed in between abnormal without disrupting the count.\
                         for HbA1c: strong considers values >47 abnormal and soft considers values >42 abnormal.")
     
@@ -52,6 +42,8 @@ def get_parser_arguments():
     parser.add_argument("--months_buffer_abnorm", type=int, help="Months to average before baseline, which if abnormal -> removed.", default=3)
     parser.add_argument("--min_age", type=float, help="Minimum age at prediction time", default=30)
     parser.add_argument("--max_age", type=float, help="Maximum age at prediction time", default=70)
+    parser.add_argument("--test_pct", type=float, help="Percentage of test data.", default=0.2)
+    parser.add_argument("--valid_pct", type=float, help="Percentage of validation data.", default=0.2)
 
     args = parser.parse_args()
 
@@ -79,73 +71,58 @@ def get_hbb_indvs(fg_ver="R13",
     )
     return(select_fids)
 
+
+def get_extra_file_descr(start_pred_date: pl.Date,
+                         end_pred_date: pl.Date,
+                         months_buffer: int,
+                         test_version: str)-> str:
+    extra = "test" + test_version
+    if start_pred_date == datetime(2022, 6, 1) and end_pred_date == datetime(2022, 12, 31):
+        extra += "_end2022"
+    elif start_pred_date == datetime(2022, 1, 1) and end_pred_date == datetime(2022, 12, 31):
+        extra += "_2022"
+    elif start_pred_date == datetime(2022, 1, 1) and end_pred_date == datetime(2024, 12, 31):
+        extra += "_2022t2024"
+    else:
+        raise("Please description of this prediction time period to the function 'get_extra_file_descr'.")
+    if months_buffer != 0:
+        extra += "_w" + str(months_buffer)
+    return(extra)
+
+
 if __name__ == "__main__":
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Initial setup                                           #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
     timer = Timer()
     args = get_parser_arguments()
-    out_file_name = args.diags_file_name.split(".")[0] + "_test_" + args.version + "_" + get_date() 
 
     make_dir(args.res_dir)
-    with open(args.res_dir + out_file_name + "_call.txt", "w") as f:
-        f.write("Time: " + get_datetime() + '\n--'.join(f'{k}={v}' for k, v in vars(args).items()))
 
     init_logging(args.res_dir, args.lab_name, logger, args)
     removed_ids_path = args.res_dir + "logs/removed_fids/" + args.lab_name + "/" 
     make_dir(removed_ids_path)
-    logging.info("Saving files to: "+out_file_name)
 
     end_pred_date = datetime.strptime(args.end_pred_date, "%Y-%m-%d")
     start_pred_date = datetime.strptime(args.start_pred_date, "%Y-%m-%d")
     base_date = start_pred_date - relativedelta(months=args.months_buffer)
     start_noabnorm_date = base_date - relativedelta(months=args.months_buffer_abnorm)
     
+    # Setting up logging
+    extra_descr = get_extra_file_descr(start_pred_date, 
+                                       end_pred_date, 
+                                       args.months_buffer, 
+                                       args.version)
+    out_file_name = args.data_path_full.split("/")[-1].split(".")[0] + "_" + extra_descr + "_" + get_date() 
+    logging_print("Saving files to: "+out_file_name)
+    with open(args.res_dir + out_file_name + "_call.txt", "w") as f:
+        f.write("Time: " + get_datetime() + '\n--'.join(f'{k}={v}' for k, v in vars(args).items()))
+
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Preparing data                                          #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    data = get_lab_data(data_path=args.data_path_full, 
-                        diags_path=args.diags_path+args.diags_file_name)
-    
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Remove "wrong" diagnoses data                           #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    data = remove_prior_diags(data=data,
-                              start_pred_date=start_pred_date,
-                              data_diag_excl=args.data_diag_excl,
-                              removed_ids_path=removed_ids_path,
-                              out_file_name=out_file_name)
-    if args.data_diag_excl == 0:
-        data = relabel_data_diag(data=data,
-                                 start_pred_date=start_pred_date,
-                                 diff_days=args.diff_days)
-    if args.shorten_diag_pred == 1:
-        data = relabel_data_diag(data=data,
-                                 start_pred_date=end_pred_date,
-                                 diff_days=args.diff_days)
-    if args.lab_name == "egfr":
-        data = remove_only_icd_or_atc_diags(data=data,
-                                            removed_ids_path=removed_ids_path,
-                                            out_file_name=out_file_name,
-                                            lab_name=args.lab_name)
-    
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Remove prior abnormal                                   #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    if args.monthss_buffer_abnorm>0:
-        data = remove_buffer_abnorm(data=data,
-                                    start_noabnorm_date=start_noabnorm_date,
-                                    base_date=base_date,
-                                    lab_name=args.lab_name,
-                                    abnorm_type=args.abnorm_type,
-                                    removed_ids_path=removed_ids_path,
-                                    out_file_name=out_file_name)
-        
-    if args.no_prior_diag_start == 1:
-        data = remove_last_value_abnormal(data=data,
-                                          removed_ids_path=removed_ids_path,
-                                          out_file_name=out_file_name)
-        
+    data = read_file(args.data_path_full)
+    print(data) 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Cases and controls                                      #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -167,17 +144,6 @@ if __name__ == "__main__":
                                  max_age=args.max_age,
                                  removed_ids_path=removed_ids_path,
                                  out_file_name=out_file_name)
-    
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Diag exclusions                                         #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-    if args.exclude_diags == 1:
-        labels = remove_other_exclusion(labels=labels,
-                                        diags_path=args.diags_path,
-                                        exclude_file_name=args.exclude_file_name,
-                                        base_date=base_date,
-                                        removed_ids_path=removed_ids_path,
-                                        out_file_name=out_file_name)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Selecting relevant individuals                          #
@@ -185,17 +151,19 @@ if __name__ == "__main__":
     # Selecting in HBB, correct age at end, not dead, and BMI >= 18.5
     select_fids = get_hbb_indvs(fg_ver=args.fg_ver, end_date=end_pred_date)
     n_nothbb = labels.filter(~pl.col("FINNGENID").is_in(select_fids)).height
-    labels = labels.filter(pl.col("FINNGENID").is_in(select_fids))
-    logging_print("Removed " + str(n_nothbb) + " individuals not in HBB.")
-    log_print_n(labels, "HBB")
+    test_labels = labels.filter(pl.col("FINNGENID").is_in(select_fids))
+    test_labels = test_labels.with_columns(pl.lit(2).alias("SET"))
+    other_labels = labels.filter(~pl.col.FINNGENID.is_in(select_fids))
+    logging_print(str(n_nothbb) + " individuals not in HBB.")
+    log_print_n(test_labels, "HBB")
+    log_print_n(other_labels, "TrainValid")
 
-    if args.lab_name=="egfr" and args.remove_low_bmi == 1:
-        select_fids = get_low_bmi_indvs(fg_ver=args.fg_ver) 
-        n_lowbmi = labels.filter(~pl.col("FINNGENID").is_in(select_fids)).height
-        logging_print("Removed " + str(n_lowbmi) + " individuals with BMI<18.5.")
-        log_print_n(labels, "BMI")
+    other_labels = add_set(other_labels, test_pct=args.test_pct, valid_pct=args.valid_pct)
+    log_print_n(other_labels.filter(pl.col.SET==1), "Valid")
+    log_print_n(other_labels.filter(pl.col.SET==0), "Train")
 
-
+    labels = pl.concat([other_labels, test_labels])
+    
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Data before start                                       #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
--- utils/labeling_utils.py
+++ utils/labeling_utils.py
@@ -13,7 +13,6 @@ def log_print_n(labels: pl.DataFrame,
     logging_print(f"MEAN N indvs {labels.height}  N cases { labels.get_column("y_MEAN_ABNORM").sum()} pct cases {round( labels.get_column("y_MEAN_ABNORM").sum()/labels.height*100,2)}%")
     logging_print(f"MIN N indvs {labels.height}  N cases { labels.get_column("y_MIN_ABNORM").sum()} pct cases {round( labels.get_column("y_MIN_ABNORM").sum()/labels.height*100,2)}%")
     logging_print(f"NEXT N indvs {labels.height}  N cases { labels.get_column("y_NEXT_ABNORM").sum()} pct cases {round( labels.get_column("y_NEXT_ABNORM").sum()/labels.height*100,2)}%")
-    logging_print(f"DIAG N indvs {labels.height}  N cases { labels.get_column("y_DIAG").sum()} pct cases {round( labels.get_column("y_DIAG").sum()/labels.height*100,2)}%")
     
 def add_ages(data: pl.DataFrame,
              date: pl.Date,
@@ -60,80 +59,6 @@ def get_low_bmi_indvs(fg_ver="R13") -> pl.Series:
     )
     return(select_fids)
 
-def remove_prior_diags(data: pl.DataFrame,
-                       start_pred_date: datetime,
-                       data_diag_excl: bool,
-                       removed_ids_path: str,
-                       out_file_name: str) -> pl.DataFrame:
-    """Remove individuals with a diagnosis before the start date of the prediction."""
-    # Remove individuals with a diagnosis before the start date of the prediction
-    data_prior_diag_removes = remove_future_diags(data, 
-                                                  start_pred_date, 
-                                                  strict=True,
-                                                  data_diag_excl=data_diag_excl)
-    # individuals with a diagnosis before the start date of the prediction
-    prior_diags_fids = data_prior_diag_removes.filter(~pl.col.FIRST_DIAG_DATE.is_null())["FINNGENID"].unique()
-    data = data.filter(~pl.col.FINNGENID.is_in(prior_diags_fids))
-
-    logging_print("Removed " + str(len(set(prior_diags_fids))) + " individuals because of prior diagnosis.")
-    pd.DataFrame({"FINNGENID":list(set(prior_diags_fids))}).to_csv(removed_ids_path + out_file_name + "_reason_prior-diag_fids.csv", sep=",")
-
-    return(data)
-
-def remove_only_icd_or_atc_diags(data: pl.DataFrame,
-                                 removed_ids_path: str,
-                                 out_file_name: str) -> pl.DataFrame:
-    # Data-diagnosis is missing but not first diagnosis
-    other_diags_fids = data.filter((pl.col.DATA_DIAG_DATE.is_null()&(~pl.col.FIRST_DIAG_DATE.is_null())))["FINNGENID"].unique()
-    data = data.filter(~pl.col.FINNGENID.is_in(other_diags_fids))
-
-    logging_print("Removed " + str(len(set(other_diags_fids))) + " individuals because of other diagosis but not data.")
-    pd.DataFrame({"FINNGENID":list(set(other_diags_fids))}).to_csv(removed_ids_path + out_file_name + "_reason_only-other-diags_fids.csv", sep=",")
-
-    return(data)
-
-def remove_buffer_abnorm(data: pl.DataFrame,
-                         start_noabnorm_date: datetime,
-                         base_date: datetime,
-                         lab_name: str,
-                         abnorm_type: str,
-                         removed_ids_path: str,
-                         out_file_name: str) -> pl.DataFrame:
-    
-    # Mean during the buffer period before baseline
-    abnorm_data = (data
-                       .filter((pl.col.DATE >= start_noabnorm_date)&(pl.col.DATE < base_date))
-                       .with_columns(pl.col.VALUE.mean().over("FINNGENID").alias("MEAN_PRIOR"))
-                       .select("FINNGENID", "MEAN_PRIOR")
-    )
-    # transform mean into abnormal
-    abnorm_data = get_abnorm_func_based_on_name(lab_name, abnorm_type)(abnorm_data, "MEAN_PRIOR")
-    # Remove individuals with abnormal mean
-    abnorm_fids = abnorm_data.filter(pl.col.ABNORM_CUSTOM>0).get_column("FINNGENID").unique()
-    data = data.filter(~pl.col.FINNGENID.is_in(abnorm_fids))
-    # Logging
-    logging_print("Removed " + str(len(set(abnorm_fids))) + " individuals because of prior mean abnormal.")
-    pd.DataFrame({"FINNGENID":list(set(abnorm_fids))}).to_csv(removed_ids_path + out_file_name + "_reason_prior-abnorm_fids.csv", sep=",")
-
-    return(data)    
-
-def remove_last_value_abnormal(data: pl.DataFrame,
-                               base_date: datetime,
-                               removed_ids_path: str,
-                               out_file_name: str) -> pl.DataFrame:
-    # Diagnosis is inside prediction period
-    # But first abnormal is not before baseline date
-    last_val_abnormal_fids = (data.filter(pl.col.DATE<base_date)
-                                  .filter(pl.col.ABNORM_CUSTOM!=0.5)
-                                  .filter(pl.col.DATE==pl.col.DATE.max().over("FINNGENID"))
-                                  .filter(pl.col.ABNORM_CUSTOM==1)
-                                  .get_column("FINNGENID")
-                                  .unique()
-    )
-    logging_print("Removed " + str(len(set(last_val_abnormal_fids))) + " individuals because of last value abnormal.")
-    pd.DataFrame({"FINNGENID":list(set(last_val_abnormal_fids))}).to_csv(removed_ids_path + out_file_name + "_reason_last-abnormal_fids.csv", sep=",")
-    data = data.filter(~pl.col.FINNGENID.is_in(last_val_abnormal_fids))
-    return(data)    
 
 def label_cases_and_controls(data: pl.DataFrame,
                              start_pred_date: datetime,
@@ -146,20 +71,17 @@ def label_cases_and_controls(data: pl.DataFrame,
     labels = (data.filter((pl.col.DATE>=start_pred_date)&(pl.col.DATE<=end_pred_date))
                 .with_columns(
                     pl.col.VALUE.mean().over("FINNGENID").alias("y_MEAN"),
-                    pl.col.VALUE.min().over("FINNGENID").alias("y_MIN"),
+                    pl.when((lab_name!="hba1c")&(lab_name!="tsh"))
+                    .then(pl.col.VALUE.min().over("FINNGENID"))
+                    .otherwise(pl.col.VALUE.max().over("FINNGENID"))
+                    .alias("y_MIN"),
                     pl.col.VALUE.get(pl.col.DATE.arg_min()).over("FINNGENID").alias("y_NEXT"),
-                    # Cases should have the diagnosis date inside the prediction period
-                    # Will not have first abnormal here, if heappened earlier but no diag yet.
-                    # Or also the first abnormal is enough here
-                    pl.when(((pl.col.DATA_FIRST_DIAG_ABNORM_DATE>=start_pred_date)&(pl.col.DATA_FIRST_DIAG_ABNORM_DATE<=end_pred_date)) | \
-                            ((pl.col.DATA_DIAG_DATE>=start_pred_date)&(pl.col.DATA_DIAG_DATE<=end_pred_date)))              
-                      .then(1).otherwise(0).alias("y_DIAG")
                 )
     )
     labels = get_abnorm_func_based_on_name(lab_name, abnorm_type)(labels, "y_MEAN").rename({"ABNORM_CUSTOM": "y_MEAN_ABNORM"})
     labels = get_abnorm_func_based_on_name(lab_name, abnorm_type)(labels, "y_MIN").rename({"ABNORM_CUSTOM": "y_MIN_ABNORM"})
     labels = get_abnorm_func_based_on_name(lab_name, abnorm_type)(labels, "y_NEXT").rename({"ABNORM_CUSTOM": "y_NEXT_ABNORM"})
-    labels = labels.select("FINNGENID", "SEX", "y_MEAN", "y_MEAN_ABNORM", "y_MIN", "y_MIN_ABNORM", "y_NEXT", "y_NEXT_ABNORM", "y_DIAG").unique()
+    labels = labels.select("FINNGENID", "SEX", "y_MEAN", "y_MEAN_ABNORM", "y_MIN", "y_MIN_ABNORM", "y_NEXT", "y_NEXT_ABNORM").unique()
     
     no_labels_data_fids = data.filter(~pl.col.FINNGENID.is_in(labels["FINNGENID"]))["FINNGENID"].unique()
     logging_print("Removed " + str(len(set(no_labels_data_fids))) + " individuals because of no measurement in the time.")
@@ -206,73 +128,6 @@ def remove_other_exclusion(labels: pl.DataFrame,
     log_print_n(labels, "Exclusion")
     return(labels)
 
-def remove_future_diags(data, 
-                        end_pred_date,
-                        strict=False,
-                        data_diag_excl=True,
-                        diff_days=0):
-    if data_diag_excl == True:
-        # Future diagnoses
-        if strict:
-            # Removing all information that leads to a diagnosis after the end_pred_date
-            # That means we also remove the first abnormal date, considering it simply
-            # a "solo" abnormal date
-            data = data.with_columns([
-                # First diagnosis after end_pred_date -> first abnormal date set to None
-                pl.when(pl.col.FIRST_DIAG_DATE>end_pred_date)
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.DATA_FIRST_DIAG_ABNORM_DATE)
-                    .alias("DATA_FIRST_DIAG_ABNORM_DATE"),
-                # First overall diagnosis after end_pred_date -> set to None
-                pl.when(pl.col.FIRST_DIAG_DATE>end_pred_date)
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.FIRST_DIAG_DATE)
-                    .alias("FIRST_DIAG_DATE"),
-                # First data-based diagnosis after end_pred_date -> set to None
-                pl.when(pl.col.DATA_DIAG_DATE>end_pred_date)
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.DATA_DIAG_DATE)
-                    .alias("DATA_DIAG_DATE")
-                ])
-        else:
-            # Removing only those individuals who also have a first measurement that is after the end_pred_date
-            # We keep individuals whos second is abnormal date is after the end_pred_date
-            # we remove first diagnosis after the end_pred_date dates without an abnormal date
-            data = data.with_columns([
-                # First abnormal leading to diagnosis OR first diagnosis without abnormal date -> set first diagnosis to None
-                pl.when((pl.col.DATA_FIRST_DIAG_ABNORM_DATE>end_pred_date)|
-                        ((pl.col.FIRST_DIAG_DATE>end_pred_date)&(pl.col.DATA_FIRST_DIAG_ABNORM_DATE.is_null())))
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.FIRST_DIAG_DATE)
-                    .alias("FIRST_DIAG_DATE"),
-                # First data-based diagnosis after end_pred_date -> set both abnorma and diagnosis date to None
-                pl.when((pl.col.DATA_FIRST_DIAG_ABNORM_DATE>end_pred_date))
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.DATA_DIAG_DATE)
-                    .alias("DATA_DIAG_DATE"),
-                pl.when(pl.col.DATA_FIRST_DIAG_ABNORM_DATE>end_pred_date)
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.DATA_FIRST_DIAG_ABNORM_DATE)
-                    .alias("DATA_FIRST_DIAG_ABNORM_DATE")
-                ])
-    else:
-        data = data.with_columns(
-                # There is a diagnosis but it is not data-based
-                pl.when(((pl.col.FIRST_ICD_DIAG_DATE.is_null())|(pl.col.FIRST_MED_DIAG_DATE.is_null()))\
-                        &(~pl.col.DATA_FIRST_DIAG_ABNORM_DATE.is_null()))
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.FIRST_DIAG_DATE)
-                    .alias("FIRST_DIAG_DATE"),
-        )
-        data = data.with_columns(
-                # but still the diagnosis is after the end_pred_date
-                pl.when((pl.col.FIRST_DIAG_DATE>end_pred_date))
-                    .then(pl.lit(None).cast(pl.Date))
-                    .otherwise(pl.col.FIRST_DIAG_DATE)
-                    .alias("FIRST_DIAG_DATE")
-        )
-
-    return(data)
 
 def get_lab_data(data_path: str, 
                  diags_path: str) -> pl.DataFrame:
@@ -293,5 +148,5 @@ def get_lab_data(data_path: str,
                     pl.col("DATA_DIAG_DATE").cast(pl.Utf8).str.to_date("%Y-%m-%d", strict=False).alias("DATA_DIAG_DATE")
 
     )
-
+    
     return(data)
