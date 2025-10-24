1b2485760362e12469158d0fe708b8b5e4dd7826
detrokir <kira.detrois@helsinki.fi>
2025-10-24 12:44:28 +0300

    add pgs

--- utils/input_utils.py
+++ utils/input_utils.py
@@ -1,4 +1,4 @@
-from general_utils import read_file
+from general_utils import read_file, print_count
 from datetime import datetime
 import polars as pl
 
@@ -25,15 +25,51 @@ def get_data_and_pred_list(file_path_labels: str,
                            file_path_sumstats: str,
                            file_path_second_sumstats: str,
                            file_path_labs: str,
+                           file_path_pgs1: str,
+                           file_path_pgs2: str,
                            preds: list,
-                           start_date: str) -> tuple[pl.DataFrame, list]:
+                           start_date: str,
+                           fill_missing: int=0) -> tuple[pl.DataFrame, list]:
     """Reads in label data and merges it with other data modalities. Returns the data and the predictors."""
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Getting Data                                            #
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     data = read_file(file_path_labels)
+    print_count(data)
 
     # Adding other data modalities
+    if file_path_pgs1 != "":
+        pgs = pl.read_csv(file_path_pgs1, separator="\t")
+        if len(pgs.columns)==2: # Zhijian PGS for eGFR levels
+            pgs.columns = ["FINNGENID", "PGS1"]
+            pgs = pgs.with_columns((-1*pl.col.PGS1).alias("PGS1"))
+        else: # Zhiyu PGS for creatinine slopes
+            pgs.columns = ["X1", "FINNGENID", "X2", "X3", "PGS1"] 
+        data = data.join(pgs.select("FINNGENID", "PGS1"), on="FINNGENID", how="left")
+        pcs = pl.read_csv("/finngen/library-red/finngen_R13/analysis_covariates/data/R13_COV_PHENO_V0.FID.txt.gz", separator="\t", columns=["IID", "PC1","PC2","PC3", "PC4","PC5","PC6","PC7","PC8","PC9","PC10"])
+        data = data.join(pcs.select("IID", "PC1","PC2","PC3", "PC4","PC5","PC6","PC7","PC8","PC9","PC10"), left_on="FINNGENID", right_on="IID", how="left")
+        print("Number of individuals without PCs")
+        print_count(data.filter(pl.col.PC1.is_null()))
+        print("Number of individuals left")
+        print_count(data.filter(~pl.col.PC1.is_null()))
+        data = data.filter(~pl.col.PC1.is_null())
+    # Adding other data modalities
+    if file_path_pgs2 != "":
+        pgs = pl.read_csv(file_path_pgs2, separator="\t")
+        if len(pgs.columns)==2: # Zhijian PGS for eGFR levels
+            pgs.columns = ["FINNGENID", "PGS2"]
+            pgs = pgs.with_columns((-1*pl.col.PGS2).alias("PGS2"))
+        else: # Zhiyu PGS for creatinine slopes
+            pgs.columns = ["X1", "FINNGENID", "X2", "X3", "PGS2"] 
+        #
+        data = data.join(pgs.select("FINNGENID", "PGS2"), on="FINNGENID", how="left")
+        pcs = pl.read_csv("/finngen/library-red/finngen_R13/analysis_covariates/data/R13_COV_PHENO_V0.FID.txt.gz", separator="\t", columns=["IID", "PC1","PC2","PC3", "PC4","PC5","PC6","PC7","PC8","PC9","PC10"])
+        data = data.join(pcs.select("IID", "PC1","PC2","PC3", "PC4","PC5","PC6","PC7","PC8","PC9","PC10"), left_on="FINNGENID", right_on="IID", how="left")
+        print("Number of individuals without PCs")
+        print_count(data.filter(pl.col.PC1.is_null()))
+        print("Number of individuals left")
+        print_count(data.filter(~pl.col.PC1.is_null()))
+        data = data.filter(~pl.col.PC1.is_null())
     if file_path_icds != "": 
         icds = read_file(file_path_icds)
         data = data.join(icds, on="FINNGENID", how="left")
@@ -48,6 +84,7 @@ def get_data_and_pred_list(file_path_labels: str,
                                      "FIRST_LAST": pl.Float64})
         if "SET" in sumstats.columns: sumstats = sumstats.drop("SET") # dropping duplicate info on set in sumstats if present
         data = data.join(sumstats, on="FINNGENID", how="left")
+        data = data.with_columns(pl.when(pl.col.MEAN.is_null()).then(pl.lit(1)).otherwise(pl.lit(0)).alias("NO_HISTORY"))
         if start_date != "":
             data = data.with_columns((datetime.strptime(start_date, "%Y-%m-%d")-pl.col.LAST_VAL_DATE).dt.total_days().alias("LAST_VAL_DIFF"))
     if file_path_second_sumstats != "": 
@@ -58,11 +95,23 @@ def get_data_and_pred_list(file_path_labels: str,
         if start_date != "":
             data = data.with_columns((datetime.strptime(start_date, "%Y-%m-%d")-pl.col.S_LAST_VAL_DATE).dt.total_days().alias("S_LAST_VAL_DIFF"))
     if file_path_labs != "": 
-        labs =read_file(file_path_labs)
+        labs  =read_file(file_path_labs)
+        null_counts = (labs.select([pl.col(c).is_null().sum().alias(c) for c in labs.columns]))
+        null_counts_filtered = null_counts.select([
+            pl.col(c) for c in null_counts.columns if null_counts[0, c] > 0
+        ])
+        print(null_counts_filtered)
         data = data.join(labs, on="FINNGENID", how="left")
+    null_counts = (data.select([pl.col(c).is_null().sum().alias(c) for c in data.columns]))
+    null_counts_filtered = null_counts.select([
+        pl.col(c) for c in null_counts.columns if null_counts[0, c] > 0
+    ])
+    print(null_counts_filtered)
     if "BMI" in preds:
-        extra_data = get_ext_data(datetime.strptime(start_date, "%Y-%m-%d"), "BMI")
+        extra_data = get_ext_data(datetime.strptime(start_date, "%Y-%m-%d"), "BMI")            
         data = data.join(extra_data.select("FINNGENID", "BMI"), how="left", on="FINNGENID")
+        if fill_missing:
+            data = data.with_columns(pl.when(pl.col.BMI.is_null()).then(pl.lit(22)).otherwise(pl.col.BMI).alias("BMI"))
     if "SMOKE" in preds:
         extra_data = get_ext_data(datetime.strptime(start_date, "%Y-%m-%d"), "SMOKE")
         data = data.join(extra_data.select("FINNGENID", "SMOKE"), how="left", on="FINNGENID")  
@@ -78,8 +127,10 @@ def get_data_and_pred_list(file_path_labels: str,
     if "EDU" in preds:
         extra_data = get_edu_data(datetime.strptime(start_date, "%Y-%m-%d"))
         data = data.join(extra_data.select("FINNGENID", "EDU"), how="left", on="FINNGENID")
+        if fill_missing:
+            data = data.with_columns(pl.when(pl.col.EDU.is_null()).then(pl.lit(0.5)).otherwise(pl.col.EDU).alias("EDU"))
     # Changing data-modality of sex column
-    data = data.with_columns(pl.col("SEX").replace({"female": 0, "male": 1}))
+    data = data.with_columns(pl.col("SEX").replace({"female": 0, "male": 1}).cast(pl.Int32).alias("SEX"))
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Predictors                                              #
@@ -102,7 +153,6 @@ def get_data_and_pred_list(file_path_labels: str,
             [X_cols.append(LAB_MAT) for LAB_MAT, _ in labs.schema.items() if LAB_MAT != "FINNGENID" and "MEAN" in LAB_MAT]
         else:
             X_cols.append(pred)
-
     return(data, X_cols)
 
 """Returns extra predictor columns to the data based on the selected predictors.
@@ -178,7 +228,7 @@ def get_ext_data(start_date: datetime,
 def get_edu_data(start_date: datetime):
     edu_data = pl.read_csv("/finngen/pipeline/finngen_R12/socio_register_1.0/data/finngen_R12_socio_register_1.0.txt.gz", separator="\t")
     edu_data = (edu_data.filter(pl.col.CATEGORY=="EDUC").with_columns(pl.col.CODE2.str.head(1).cast(pl.Int32).alias("EDU"))
-                    .filter(pl.col.YEAR<=start_date.dt.year())
+                    .filter(pl.col.YEAR<=start_date.year)
                     .filter((pl.col.EDU==pl.col.EDU.max()).over("FINNGENID"))
                     .select("FINNGENID", "EDU")
                     .unique()
