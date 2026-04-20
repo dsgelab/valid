f3f7a51109c4dd967cb3074c93699cacafa983a8
detrokir <kira.detrois@helsinki.fi>
2026-04-20 13:35:41 +0300

    added option to loop for memory save

--- data_processing/step4_atcsicds.py
+++ data_processing/step4_atcsicds.py
@@ -3,44 +3,35 @@ import sys
 
 sys.path.append(("/home/ivm/valid/scripts/utils/"))
 from general_utils import get_date, init_logging, Timer, read_file, logging_print
+from input_utils import get_all_indvs
 
 # Standard stuff
 import polars as pl
+import pandas as pd
 import argparse
 # Logging
 import logging
 logger = logging.getLogger(__name__)
 from datetime import datetime
 
-def get_out_file_name(file_name_start: str,
-                      lab_name: str,
-                      col_name: str,
-                      time: int,
+def get_out_file_name(col_name: str,
                       bin_count: int,
-                      months_before: int,
                       start_year: int,
                       min_pct: str,
-                     start_date: str) -> str:
+                      start_date: str) -> str:
     """
     Function to get the output file name based on the input parameters.
     """
 
-    out_file_name = file_name_start
     if col_name == "ICD_THREE":
-        out_file_name += "_icds"
+        out_file_name = "icds"
     elif col_name == "ATC_FIVE":
-        out_file_name += "_atcs"
-    out_file_name += "_"+min_pct+"pct"
+        out_file_name = "atcs"
+    out_file_name += "_"+str(int(min_pct))+"pct"
     if bin_count == 1:
         out_file_name += "_bin"
     else:
         out_file_name += "_count"
-    if time == 1:
-        out_file_name += "_current"
-    elif time == -1:
-        out_file_name += "_historical"
-    if months_before > 0:
-        out_file_name += "_buffer" + str(months_before) + "months"
     if start_year > 0:
         out_file_name += "_start_" + str(start_year)
     if start_date != "": 
@@ -50,32 +41,28 @@ def get_out_file_name(file_name_start: str,
     
     return out_file_name
 
-def get_time_period_data(file_path_long: str,
-                         preds_data: pl.DataFrame, 
-                         time: int, 
-                         months_before: int) -> pl.DataFrame:
+def prep_preds_data(preds_data: pl.DataFrame,
+                    start_date: str,
+                    start_year: int) -> pl.DataFrame:
     """
-    Function to get the time period data based on the input parameters.
-        historical - all predictor information only collected before first measurement -X months
-        current - all predictor information only collected after first measurement -X months
+    Function to prepare the predictor data based on the input parameters.
     """
-    # Need date of first measurement of each individual to filter historical or current data
-    start_dates = read_file(file_path_long)
-    start_dates = (start_dates
-        .sort(["FINNGENID", "DATE"])
-        .agg(pl.col.DATE==pl.col.DATE.first().over("FINNGENID").alias("DATA_START_DATE"))  # Date of first recorded measurement for each individual
-    )
-    # Start date of information X-months before first measurement
-    start_dates = start_dates.with_columns(
-        (pl.col.DATA_START_DATE - pl.duration(months=months_before)).alias("DATA_START_DATE")
-    )
-    # Adding info to predictor data
-    preds_data = preds_data.join(start_dates, on="FINNGENID", how="left")
-    # Filtering out only current or historical data
-    if time == 1:
-        preds_data = preds_data.filter(pl.col("DATE") >= pl.col("DATA_START_DATE")).drop("DATA_START_DATE")
-    elif time == -1:
-        preds_data = preds_data.filter(pl.col("DATE") <= pl.col("DATA_START_DATE")).drop("DATA_START_DATE")
+    # If date column string
+    if preds_data["DATE"].dtype == pl.Utf8:
+        preds_data = preds_data.with_columns(pl.col("DATE").str.to_date().alias("DATE"))
+
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+    #                 Filter to right time window                             #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
+    try:
+        preds_data = preds_data.filter(pl.col.DATE < datetime.strptime(start_date, "%Y-%m-%d"))
+    except:
+        preds_data = preds_data.filter(pl.col.DATE < datetime.strptime(start_date, "%Y-%m-%d %H:%M:%S"))
+
+    # Filtering out data only after what we set as start year
+    if start_year != 0:
+        preds_data = preds_data.filter(pl.col.DATE.dt.year() >= start_year)
+
     return preds_data
 
 def get_parser_arguments():
@@ -86,17 +73,16 @@ def get_parser_arguments():
     parser.add_argument("--file_path_preds", type=str, help="Full path to the longitduinal ICD or ATC data.", required=True)
     parser.add_argument("--dir_path_labels", type=str, help="Path to directory with label data.", required=True)
     parser.add_argument("--file_name_labels_start", type=str, help="Path to the diagnosis data file", default="")
-    parser.add_argument("--lab_name", type=str, help="Readable name of the measurement value for file naming.", required=True)
 
     # Settings
-    parser.add_argument("--min_pct", type=str, help="Min percentage of inclusion for file name.", required=True)
+    parser.add_argument("--min_pct", type=int, help="Min percentage of inclusion for file name. Percentage 0-100.", required=True)
     parser.add_argument("--col_name", type=str, help="Name of the column to use from the longitduinal data. [i.e. ATC_FIVE or ICD_THREE]", required=True)
-    parser.add_argument("--time", type=int, default=0, help="Whether to filter for current and not historical data. -1 = historical, 0 = all, 1 = current")
     parser.add_argument("--bin_count", type=int, default=1, help="Whether to count number of occurance or stay binary observed/not observed.")
-    parser.add_argument("--months_before", type=int, default=0, help="Months to add before start of measurements for a buffer.")
     parser.add_argument("--start_year", type=int, default=0, help="What year to start the data from. Ignored if 0.")
     parser.add_argument("--start_date", type=str, default="", help="Date to filter before", required=False)
-    parser.add_argument("--select_icds_path", type=str, default="", help="Path to processed ICD codes. To do batched work.", required=False)
+    parser.add_argument("--fg_ver", type=str, default="R14", help="Version/SB. Can be R12-14 or ml4h.", required=False)
+
+    parser.add_argument("--select_code_paths", type=str, default="", help="List of paths to selected code files.", required=False)
 
     # Settings
     args = parser.parse_args()
@@ -109,51 +95,21 @@ if __name__ == "__main__":
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
     timer = Timer()
     args = get_parser_arguments()
-    out_file_name = get_out_file_name(args.file_name_labels_start, 
-                                      args.lab_name, 
-                                      args.col_name, 
-                                      args.time, 
+    out_file_name = get_out_file_name(args.col_name, 
                                       args.bin_count, 
-                                      args.months_before, 
                                       args.start_year,
                                       args.min_pct,
                                       args.start_date)
-    init_logging(args.res_dir, args.lab_name, logger, args)
+    init_logging(args.res_dir, "all", logger, args)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Get data                                                #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
-    preds_data = read_file(args.file_path_preds)
-
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Filter to right time window                             #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
-    if args.time != 0:
-        preds_data = get_time_period_data(args.dir_path_labels+args.file_name_labels_start+".parquet",
-                                          preds_data,
-                                          args.time,
-                                          args.months_before)
-    labels = read_file(args.dir_path_labels+args.file_name_labels_start+"_labels.parquet")
-
-    if args.start_date == "": 
-        # Filtering out data before start of prediction   
-        preds_data = preds_data.join(labels.select("FINNGENID", "START_DATE"), on="FINNGENID", how="left")
-        preds_data = preds_data.filter(pl.col.DATE < pl.col.START_DATE).drop("START_DATE")
-    else:
-        try:
-            preds_data = preds_data.filter(pl.col.DATE < datetime.strptime(args.start_date, "%Y-%m-%d"))
-        except:
-            preds_data = preds_data.filter(pl.col.DATE < datetime.strptime(args.start_date, "%Y-%m-%d %H:%M:%S"))
-
-    # Filtering out data only after what we set as start year
-    if args.start_year != 0:
-        preds_data = preds_data.filter(pl.col.DATE.dt.year() >= args.start_year)
-
-    if args.select_icds_path == "":
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-        #                 Counting and wide in one go                             #
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
+    #                 Counting and wide in one go                             #
+    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
+    if args.select_code_paths == "":
+        preds_data = read_file(args.file_path_preds)
+        preds_data = prep_preds_data(preds_data=preds_data,
+                                     start_date=args.start_date,
+                                     start_year=args.start_year) 
         preds_wider = (preds_data
                         .group_by(["FINNGENID", args.col_name])
                         .agg(pl.len().alias("N_CODE"))
@@ -163,21 +119,27 @@ if __name__ == "__main__":
         if args.bin_count == 1:
             preds_wider = preds_wider.with_columns(pl.when(pl.col.N_CODE>=1).then(1).otherwise(0).alias("N_CODE"))
 
-        # Making wide - problem with large data just stops
         preds_wider = (preds_wider
                         .pivot(values="N_CODE", index="FINNGENID", on=args.col_name)
         )
-
-        # Add people without any codes
-        preds_wider = preds_wider.join(labels[["FINNGENID"]], on="FINNGENID", how="full", coalesce=True)
-        preds_wider = preds_wider.fill_null(0)
-        select_icds = read_file(args.select_icds_path).sort(args.col_name).get_column(args.col_name)
+    else:
+        select_icds = (read_file(args.select_code_paths)
+                       .filter(pl.col.PCT_INDV >= float(args.min_pct/100))
+                       .sort(args.col_name)
+                       .get_column(args.col_name)
+                       )
+        
         preds_wider = pl.DataFrame()
         total_timer = Timer()
         for code in select_icds:
             code_timer = Timer()
-            code_data = preds_data.filter(pl.col(args.col_name) == code).select(["FINNGENID", "DATE"])
+
+            code_data = pl.DataFrame(pd.read_parquet(args.file_path_preds, filters=[(f"{args.col_name}", "==", code)]))
+            if "FINNGENID" not in code_data.columns:
+                code_data = code_data.rename({"FID": "FINNGENID"})
+            
             code_data = code_data.group_by("FINNGENID").agg(pl.len().alias("N_CODE"))
+
             if args.bin_count == 1:
                 code_data = code_data.with_columns(pl.when(pl.col.N_CODE>=1).then(1).otherwise(0).alias("N_CODE"))
             code_data = code_data.rename({"N_CODE": code})
@@ -185,24 +147,16 @@ if __name__ == "__main__":
                 preds_wider = code_data
             else:
                 preds_wider = preds_wider.join(code_data, on="FINNGENID", how="full", coalesce=True)
-            logging_print("Time for code: " + str(code_timer.time()))
+            logging_print("Time for code: " + code + " took " + str(code_timer.get_elapsed()))
         preds_wider = preds_wider.fill_null(0)
         logging_print(f"Total time for looping through omops: {total_timer.get_elapsed()}")
+
+    # Add people without any codes
+    all_indvs = get_all_indvs(args.fg_ver)
+
+    preds_wider = preds_wider.join(all_indvs[["FINNGENID"]], on="FINNGENID", how="full", coalesce=True)
+    preds_wider = preds_wider.fill_null(0)
     print(preds_wider)
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Date of last code                                       #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
-    last_date = (preds_data
-                    .select(["FINNGENID", "DATE"])
-                    .unique()
-                    .sort(["FINNGENID", "DATE"], descending=True)
-                    .group_by("FINNGENID")
-                    .agg(pl.col("DATE").first().alias("LAST_CODE_DATE"))
-    )
-
-    # Adding info to predictor data
-    preds_wider = preds_wider.join(last_date, on="FINNGENID", how="left")
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Saving                                                  #
--- data_processing/step4_labs.py
+++ data_processing/step4_labs.py
@@ -19,15 +19,11 @@ def get_parser_arguments():
     parser = argparse.ArgumentParser()
     # Saving info
     parser.add_argument("--res_dir", type=str, help="Path to the results directory", required=True)
-    parser.add_argument("--file_path_lab", type=str, help="Full üath to the results lab measurement data.", required=True)
-    parser.add_argument("--dir_path_labels", type=str, help="Path to directory with label data.", required=True)
-    parser.add_argument("--file_name_labels_start", type=str, help="Path to the diagnosis data file", default="")
-    parser.add_argument("--lab_name", type=str, help="Readable name of the measurement value for file naming.", required=True)
+    parser.add_argument("--file_path_lab", type=str, help="Full path to the results lab measurement data.", required=True)
     parser.add_argument("--start_date", type=str, default="", help="Date to filter before", required=False)
-    parser.add_argument("--clean", type=int, default=0, help="How many predictors to remove.", required=False)
-    parser.add_argument("--mean_impute", type=int, default=0, help="Impute mean for each OMOP if missing for individual.", required=False)
     parser.add_argument("--egfr_data_path", type=str, default="/home/ivm/valid/data/processed_data/step1_clean/egfr_d1_KDIGO-soft_ld_2025-08-13.parquet", help="Path to processed eGFR data.", required=False)
     parser.add_argument("--select_omops_path", type=str, default="", help="Path to processed OMOP concept IDs data. To do batched work.", required=False)
+    parser.add_argument("--fg_ver", type=str, default="R14", help="Version/SB. Can be R12-14 or ml4h.", required=False)
 
     # Settings
     args = parser.parse_args()
@@ -41,48 +37,13 @@ if __name__ == "__main__":
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
     timer = Timer()
     args = get_parser_arguments()
-    init_logging(args.res_dir, args.lab_name, logger, args)
-    out_file_name = args.file_name_labels_start+"_labs_"
-    if args.mean_impute == 1:
-        out_file_name += "impute_"
-    if args.clean == 1:
-        out_file_name += "clean_"
+    init_logging(args.res_dir, "all", logger, args)
+
+    out_file_name = "labs_"
     if args.start_date != "": 
         out_file_name = out_file_name + "pred" + str(int(args.start_date[0:4])+1) + "_"
     out_file_name += get_date()
     print(out_file_name)
-
-    labels = read_file(args.dir_path_labels+args.file_name_labels_start+"_labels.parquet")
-
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-    #                 Duplicate predictors                           #
-    # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #  
-    if args.lab_name == "krea" or args.lab_name == "egfr":
-        if args.clean == 1:
-            # krea, egfr some other formula, egfr, cystatin c, UACR
-            not_select_omops = ["40764999", "46236952", "3020564", "3030366", "3020682"]
-        else:
-            not_select_omops = ["40764999", "46236952", "3020564"]
-    if args.lab_name == "hba1c":
-        if args.clean == 1:
-            not_select_omops = ["3004410", "3018251", "3013826", "3013826"]# hba1c and fasting glucose, glucose, glucose 2 hours post dose
-        else:
-            not_select_omops = ["3004410", "3018251"] # hba1c and fasting glucose
-    if args.lab_name == "alatasat":
-        # ALAT, ASATs
-        not_select_omops = ["3006923", "3013721"]
-    if args.lab_name == "tsh":
-        if args.clean == 1:
-            not_select_omops = ["3009201", "3008486", "3026989"] # tsh, t4, t3
-        else:
-            not_select_omops = ["3009201", "3008486"]
-    if args.lab_name == "ldl":
-        if args.clean == 1: 
-            # LDL, LDL/HDL, HDl standard, HDL total, cholesterol total, weird cholesterol non hdl rare, tri fast, tri
-            not_select_omops = ["3001308", "3019900", "3023602", "42868674","3019900", "3048773", "3025839"]
-        else:
-            # LDL, tryg free, tryg
-            not_select_omops = ["3001308", "3048773", "3025839"]
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Get data                                                #
@@ -93,34 +54,29 @@ if __name__ == "__main__":
                                         pl.col.OMOP_CONCEPT_ID.cast(pl.Utf8))
         if labs_data["APPROX_EVENT_DATETIME"].dtype == pl.Utf8:
             labs_data = labs_data.with_columns(pl.col.APPROX_EVENT_DATETIME.str.to_date("%Y-%m-%dT%H:%M", strict=False))
+
         # Adding manual eGFR data 
-        if args.lab_name not in ["krea", "egfr"]:
-            crea_data = read_file(args.egfr_data_path)
-            crea_data = (crea_data
+        crea_data = read_file(args.egfr_data_path)
+        crea_data = (crea_data
                         .rename({"VALUE": "MEASUREMENT_VALUE", "DATE": "APPROX_EVENT_DATETIME"})
                         .with_columns(pl.Series("OMOP_CONCEPT_ID", ["40764999"]*crea_data.height),
                                     pl.col.APPROX_EVENT_DATETIME.dt.date().alias("APPROX_EVENT_DATETIME"))
                         )
-            labs_data = (labs_data
+        labs_data = (labs_data
                         .filter(~pl.col.OMOP_CONCEPT_ID.is_in(["40764999", "3020564", "46236952"]))
                         .with_columns(pl.col.APPROX_EVENT_DATETIME.dt.date().alias("APPROX_EVENT_DATETIME"))
                         )
-            labs_data = pl.concat([labs_data.select("FINNGENID", "MEASUREMENT_VALUE", "APPROX_EVENT_DATETIME", "OMOP_CONCEPT_ID"),
-                                    crea_data.select("FINNGENID", "MEASUREMENT_VALUE", "APPROX_EVENT_DATETIME", "OMOP_CONCEPT_ID")])
-        labs_data = labs_data.filter(~pl.col.OMOP_CONCEPT_ID.is_in(not_select_omops))
+        labs_data = pl.concat([labs_data.select("FINNGENID", "MEASUREMENT_VALUE", "APPROX_EVENT_DATETIME", "OMOP_CONCEPT_ID"),
+                               crea_data.select("FINNGENID", "MEASUREMENT_VALUE", "APPROX_EVENT_DATETIME", "OMOP_CONCEPT_ID")])
     
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
         #                 Filter to before start of prediction                    #
         # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
         print(labs_data)
-        if args.start_date == "": 
-            labs_data = labs_data.join(labels.select("FINNGENID"), on="FINNGENID", how="right")
-            labs_data = labs_data.filter(pl.col.APPROX_EVENT_DATETIME < pl.col.START_DATE)
-        else:
-            try:
-                labs_data = labs_data.filter(pl.col.APPROX_EVENT_DATETIME < datetime.strptime(args.start_date, "%Y-%m-%d"))
-            except:
-                labs_data = labs_data.filter(pl.col.APPROX_EVENT_DATETIME < datetime.strptime(args.start_date, "%Y-%m-%d %H:%M:%S"))
+        try:
+            labs_data = labs_data.filter(pl.col.APPROX_EVENT_DATETIME < datetime.strptime(args.start_date, "%Y-%m-%d"))
+        except:
+            labs_data = labs_data.filter(pl.col.APPROX_EVENT_DATETIME < datetime.strptime(args.start_date, "%Y-%m-%d %H:%M:%S"))
 
 
                 
@@ -145,7 +101,7 @@ if __name__ == "__main__":
         })
     else:
         select_omops = (read_file(args.select_omops_path)
-                         .filter(~pl.col.OMOP_ID.cast(pl.Utf8).is_in(not_select_omops), pl.col.OMOP_ID>0)
+                         .filter(pl.col.OMOP_ID>0)
                          # Removing NA units if there is also one without NA
                          .sort("OMOP_ID", "N").group_by("OMOP_ID", maintain_order=True).last()
                         )
@@ -176,23 +132,6 @@ if __name__ == "__main__":
                 labs_data = labs_data.join(crnt_labs_data, on="FINNGENID", how="full", coalesce=True)
             print(f"{crnt_omop} took {timer.get_elapsed()} have {n_rows} rows and N={crnt_labs_data.height} individuals")
         logging_print(f"Total time for looping through omops: {total_timer.get_elapsed()}")
-    if args.mean_impute == 1:
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
-        #                 Adding individuals with no measurements                 #
-        # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #   
-        labs_data = labs_data.join(labels.select("FINNGENID", "SET"), on="FINNGENID", how="right")
-    
-        print_count(labs_data)
-        # Mean impute with help of co-pilot 16.9.25
-        col_means = {
-            col: labs_data.filter(pl.col.SET==0)[col].mean()
-            for col in labs_data.columns if col != "FINNGENID"
-        }
-        # Fill missing values with column means
-        labs_data = labs_data.with_columns([
-            pl.col(col).fill_null(col_means[col]).alias(col) for col in labs_data.columns if col != "FINNGENID"
-        ]).drop("SET")
-    print(labs_data)
 
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
     #                 Saving                                                  #
